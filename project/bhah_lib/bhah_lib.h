e34ae6c54c6e2fabe9c83863e2c4156528f3c65a
Thiago Assumpção <assumpcaothiago@gmail.com>
2024-11-18 11:36:01 -0600

    project/bhah_lib/bhah_lib.h: include BHaH_set_TOV().

--- project/bhah_lib/bhah_lib.h
+++ project/bhah_lib/bhah_lib.h
@@ -11,12 +11,13 @@ void BHaH_setup(const int nxx0, const int nxx1, const int nxx2,
 // double BHaH_get_timestep(BHaH_struct *bhahstruct); // this one should be deprecated
 // void BHaH_advance_timestep(const REAL t_final, BHaH_struct *bhahstruct);
 void BHaH_get_metric_extrinsic_curvature(BHaH_struct *bhahstruct, REAL Cartx, REAL Carty, REAL Cartz,
-                                         REAL *alpha_out, REAL *beta0, REAL *beta1, REAL *beta2, 
+                                         REAL *alpha_out, REAL *beta0, REAL *beta1, REAL *beta2,
                                          REAL gammaDD[3][3], REAL KDD[3][3]);
 // void BHaH_set_Tmunu(const int rgrid, const REAL rmax, REAL *in_Tmunu, BHaH_struct *bhahstruct);  // this function will not be inclulded since it is not used.
 void BHaH_output_file(char *filename, BHaH_struct *bhahstruct, int dim);
 int BHaH_get_gridpoints(int *indices, REAL *xCartGrid, REAL xCartMax[3], BHaH_struct *bhahstruct);
 void BHaH_set_Tmunu_gridpoints(const int nCartGrid, int *indices, REAL *TmunuGrid, BHaH_struct *bhahstruct);
+void BHaH_set_TOV(BHaH_struct *bhahstruct);
 
 #ifdef __cplusplus
 }
